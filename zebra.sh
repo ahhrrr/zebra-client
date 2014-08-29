d9a417c568509a872acee251622d23919e31ad02
Ezra Spier <ezra.spier@gmail.com>
2014-08-29 14:30:02 -0700

    HTTP server for printing.

--- zebra.sh
+++ zebra.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source .env
-echo "Resetting label size to 4x6 on $ZEBRA_PRINT_QUEUE_NAME"
-lpr -P $ZEBRA_PRINT_QUEUE_NAME -o raw < reset-label-size.zpl
+#echo "Resetting label size to 4x6 on $ZEBRA_PRINT_QUEUE_NAME"
+#lpr -P $ZEBRA_PRINT_QUEUE_NAME -o raw < reset-label-size.zpl
 
 until coffee zebra.js.coffee; do
     echo "Zebra server crashed with exit code $?.  Respawning.." >&2
